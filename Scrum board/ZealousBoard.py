8dbdfd59b58d9e733e7bed6b9366cd47ee058e1f
razzaksr <razzaksr@gmail.com>
2025-02-20 15:29:21 +0530

    CRUD with Mongodb done

--- Scrum board/ZealousBoard.py
+++ Scrum board/ZealousBoard.py
@@ -2,64 +2,97 @@ from Task import *
 from dotenv import *
 import os
 from pymongo import *
+import json
 
 class ZealousBoard:
     '''data with CRUD'''
     buckets={
         "todo":[],"progress":[],"review":[],"done":[]
     }
+    # CRUD operations: create task, update task, delete task, read task
     def __init__(self):
         load_dotenv()
-        uri = mongodb+srv://os.getenv('user'):os.getenv('password')@cluster0.ptmlylq.mongodb.net/kongunadu?retryWrites=true&w=majority&appName=Cluster0
-        client = MongoClient(uri)
-        collection = client.kongunadu
-    # CRUD operations: create task, update task, delete task, read task
+        self.uri = "mongodb+srv://"+os.getenv('user')+":"+os.getenv('password')+"@cluster0.ptmlylq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        self.client = MongoClient(self.uri)
+        self.db = self.client.kongunadu
+        self.collection = self.db.task
+    def load(self):
+        bsons = self.collection.find()
+        for bson in bsons:
+            # json to object
+            # print(bson)
+            task = Task(bson['taskId'],bson['taskName'],bson['taskAssigner'],bson['taskAssignee'],bson['taskStoryPoints'],bson['taskBucket'])
+            if task.taskBucket == 'todo':
+                self.buckets['todo'].append(task)
+            elif task.taskBucket == 'progress':
+                self.buckets['progress'].append(task)
+            elif task.taskBucket == 'review':
+                self.buckets['review'].append(task)
+            elif task.taskBucket == 'done':
+                self.buckets['done'].append(task)
     
     def viewBoard(self):
+        # read all from mongodb
+        self.load()
         for bucket in self.buckets.keys():
             print(bucket,"bucket task's of ")
             for x in self.buckets[bucket]:
                     print(str(x))
+        
     
     # adding task to bucket>> Create
     def addToBucket(self,task,bucket):
-        if bucket == 'todo' and task.taskBucket=='todo':
-            self.buckets['todo'].append(task)
-            print(task.taskId," included in the ",bucket)
-        elif bucket == "progress" and task.taskBucket =="progress":
-            for x in range(len(self.buckets['todo'])):
-                if self.buckets['todo'][x].taskId==task.taskId:
-                    # print("To be removed ",str(self.buckets['todo'][x]))
-                    self.buckets['todo'].remove(self.buckets['todo'][x])
-                    self.buckets['progress'].append(task)
-                    print(task.taskId," has been moved to "+task.taskBucket)
-                    return
-            else:
-                print('Invalid task/bucket')
-        elif bucket == "review" and task.taskBucket =="review":
-            for x in range(len(self.buckets['progress'])):
-                if self.buckets['progress'][x].taskId==task.taskId:
-                    # print("To be removed ",str(self.buckets['progress'][x]))
-                    self.buckets['progress'].remove(self.buckets['progress'][x])
-                    self.buckets['review'].append(task)
-                    print(task.taskId," has been moved to "+task.taskBucket)
-                    return
-            else:
-                print('Invalid task/bucket')
-        elif bucket == "done" and task.taskBucket =="done":
-            for x in range(len(self.buckets['review'])):
-                if self.buckets['review'][x].taskId==task.taskId:
-                    # print("To be removed ",str(self.buckets['review'][x]))
-                    self.buckets['review'].remove(self.buckets['review'][x])
-                    self.buckets['done'].append(task)
-                    print(task.taskId," has been moved to "+task.taskBucket)
-                    return
-            else:
-                print('Invalid task/bucket')
+        jsonTask = {
+            "taskId":task.taskId,
+            "taskName":task.taskName,
+            "taskAssigner":task.taskAssigner,
+            "taskAssignee":task.taskAssignee,
+            "taskStoryPoints":task.taskStoryPoints,
+            "taskBucket":task.taskBucket
+        }
+        print(jsonTask)
+        self.collection.insert_one(jsonTask);
+        print(task.taskId," included in the ",bucket)
+        # if bucket == 'todo' and task.taskBucket=='todo':
+        #     # self.buckets['todo'].append(task)
+        #     jsonTask = json.dump(task)
+        #     self.collection.insert_one(jsonTask);
+        #     print(task.taskId," included in the ",bucket)
+        # elif bucket == "progress" and task.taskBucket =="progress":
+        #     for x in range(len(self.buckets['todo'])):
+        #         if self.buckets['todo'][x].taskId==task.taskId:
+        #             # print("To be removed ",str(self.buckets['todo'][x]))
+        #             self.buckets['todo'].remove(self.buckets['todo'][x])
+        #             self.buckets['progress'].append(task)
+        #             print(task.taskId," has been moved to "+task.taskBucket)
+        #             return
+        #     else:
+        #         print('Invalid task/bucket')
+        # elif bucket == "review" and task.taskBucket =="review":
+        #     for x in range(len(self.buckets['progress'])):
+        #         if self.buckets['progress'][x].taskId==task.taskId:
+        #             # print("To be removed ",str(self.buckets['progress'][x]))
+        #             self.buckets['progress'].remove(self.buckets['progress'][x])
+        #             self.buckets['review'].append(task)
+        #             print(task.taskId," has been moved to "+task.taskBucket)
+        #             return
+        #     else:
+        #         print('Invalid task/bucket')
+        # elif bucket == "done" and task.taskBucket =="done":
+        #     for x in range(len(self.buckets['review'])):
+        #         if self.buckets['review'][x].taskId==task.taskId:
+        #             # print("To be removed ",str(self.buckets['review'][x]))
+        #             self.buckets['review'].remove(self.buckets['review'][x])
+        #             self.buckets['done'].append(task)
+        #             print(task.taskId," has been moved to "+task.taskBucket)
+        #             return
+        #     else:
+        #         print('Invalid task/bucket')
             
     
     def readFromBucket(self,bucket):
         #print(bucket)
+        self.load()
         if bucket == 'todo' or bucket == 'progress' or bucket == 'review' or bucket == 'done':
             print("The task in the "+bucket+" buckets are")
             for x in self.buckets[bucket]:
@@ -68,6 +101,7 @@ class ZealousBoard:
             print("invalid bucket")
     
     def readByEmployee(self,name):
+        self.load()
         for bucket in self.buckets.keys():
             print(bucket,"bucket task's of ",name)
             for x in self.buckets[bucket]:
